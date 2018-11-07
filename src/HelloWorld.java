2b670378a5b2b0c525f1d0c6fc8cdae69d8fccaf
KennethMcInturf <kennethcmcinturf@gmail.com>
2018-11-07 09:20:48 -0600

    Messing with intergers

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,25 @@
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello World");
+//        System.out.print("Hello ");
+//        System.out.print("World");
+        int myFavoriteNumber = 13;
+        System.out.println(myFavoriteNumber);
+        String myString = "Panthers are the best";
+        System.out.println(myString);
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        int three = (int) "three";
+        int x = 10;
+        int y = 2;
+        x = x / y;
+        y = y - x;
+        System.out.println(x);
+        System.out.println(y);
     }
 }
